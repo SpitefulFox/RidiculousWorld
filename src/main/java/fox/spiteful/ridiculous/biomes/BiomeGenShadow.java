1e648f91b88cd56c3885b2756b041dba511b600c
Jeremy Diamond <diamondjbj2@yahoo.com>
2014-11-15 20:33:16 -0800

    Less Laggy Shadow Generation

--- src/main/java/fox/spiteful/ridiculous/biomes/BiomeGenShadow.java
+++ src/main/java/fox/spiteful/ridiculous/biomes/BiomeGenShadow.java
@@ -5,6 +5,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 import fox.spiteful.ridiculous.Config;
 import fox.spiteful.ridiculous.Lumberjack;
 import fox.spiteful.ridiculous.blocks.RidiculousBlocks;
+import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
@@ -19,15 +20,15 @@ public class BiomeGenShadow extends BiomeGenBase {
     BiomeGenShadow(int id){
         super(id);
         this.theBiomeDecorator.treesPerChunk = 2;
-        this.theBiomeDecorator.flowersPerChunk = 1;
-        this.theBiomeDecorator.deadBushPerChunk = 1;
+        this.theBiomeDecorator.flowersPerChunk = 0;
+        this.theBiomeDecorator.deadBushPerChunk = 4;
         this.theBiomeDecorator.mushroomsPerChunk = 8;
-        this.theBiomeDecorator.reedsPerChunk = 10;
+        this.theBiomeDecorator.reedsPerChunk = 0;
         this.theBiomeDecorator.clayPerChunk = 1;
-        this.theBiomeDecorator.waterlilyPerChunk = 4;
+        this.theBiomeDecorator.waterlilyPerChunk = 0;
         this.theBiomeDecorator.sandPerChunk2 = 0;
         this.theBiomeDecorator.sandPerChunk = 0;
-        this.theBiomeDecorator.grassPerChunk = 5;
+        this.theBiomeDecorator.grassPerChunk = 8;
         this.waterColorMultiplier = 0x00002F;
         this.flowers.clear();
         BiomeManager.addBiome(BiomeManager.BiomeType.COOL, new BiomeManager.BiomeEntry(this, Config.shadowWeight));
@@ -61,19 +62,31 @@ public class BiomeGenShadow extends BiomeGenBase {
         return 0x000064;
     }
 
-    public void decorate(World world, Random rand, int chunkX, int chunkZ)
+    /*public void decorate(World world, Random rand, int chunkX, int chunkZ)
     {
         //int y = world.getHeight() - 1;
         int y = 90;
         //Lumberjack.log(Level.INFO, chunkX + ", " + chunkZ);
         //world.setBlock(chunkX, y, chunkZ, Blocks.bedrock, 0, 2);
-        for(int x = chunkX;x < chunkX + 16;x++){
-            for(int z = chunkZ;z < chunkZ + 16;z++){
-                world.setBlock(x, y, z, RidiculousBlocks.gloom, 0, 2);
-                //world.setBlock(x, y, z, Blocks.bedrock, 0, 2);
+        int maxX = chunkX + 24;
+        for(int x = chunkX + 8;x < maxX;x++){
+            int maxZ = chunkZ + 24;
+            for(int z = chunkZ + 8;z < maxZ;z++){
+                //world.setBlock(x, y, z, RidiculousBlocks.gloom, 0, 2);
+                world.setBlock(x, y, z, Blocks.bedrock, 0, 0);
             }
         }
 
         this.theBiomeDecorator.decorateChunk(world, rand, this, chunkX, chunkZ);
+    }*/
+
+    public void genTerrainBlocks(World world, Random rand, Block[] chunk, byte[] metadatamaybe, int xMaybe, int zMaybe, double wat)
+    {
+        this.genBiomeTerrain(world, rand, chunk, metadatamaybe, xMaybe, zMaybe, wat);
+
+        for (int wut = 90; wut < chunk.length; wut+=256) {
+            chunk[wut] = RidiculousBlocks.gloom;
+            metadatamaybe[wut] = 0;
+        }
     }
 }
