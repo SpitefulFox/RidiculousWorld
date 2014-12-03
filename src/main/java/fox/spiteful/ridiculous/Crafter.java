27b863172a63700e8b3ac9a700ccfa3460390586
Jeremy Diamond <diamondjbj2@yahoo.com>
2014-12-03 08:04:39 -0800

    Recipes

--- src/main/java/fox/spiteful/ridiculous/Crafter.java
+++ src/main/java/fox/spiteful/ridiculous/Crafter.java
@@ -19,19 +19,16 @@ public class Crafter {
 
     public static void scissors(){
 
-        GameRegistry.addShapelessRecipe(new ItemStack(RidiculousBlocks.treePlanks, 4, 0), new Object[] {new ItemStack(RidiculousBlocks.treeLogs,1,0)});
-        GameRegistry.addShapelessRecipe(new ItemStack(RidiculousBlocks.treePlanks, 4, 0), new Object[] {new ItemStack(RidiculousBlocks.spookyLog,1,0)});
-        GameRegistry.addShapelessRecipe(new ItemStack(RidiculousBlocks.treePlanks, 4, 1), new Object[] {new ItemStack(RidiculousBlocks.treeLogs,1,1)});
-        GameRegistry.addShapedRecipe(new ItemStack(RidiculousBlocks.craftBench, 1, 0), new Object[] {"XX", "XX", 'X', new ItemStack(RidiculousBlocks.treePlanks, 1, 0)});
-
         GameRegistry.addSmelting(new ItemStack(RidiculousItems.unicornRaw, 1), new ItemStack(RidiculousItems.unicornCooked, 1), 0.35F);
         GameRegistry.addSmelting(new ItemStack(RidiculousItems.peepRaw, 1), new ItemStack(RidiculousItems.peepCooked, 1), 0.35F);
 
         for(int x = 0;x < BlockFantasyLog.types.length;x++) {
             GameRegistry.addSmelting(new ItemStack(RidiculousBlocks.treeLogs, 1, x), new ItemStack(Items.coal, 1, 1), 0.15F);
+            GameRegistry.addShapelessRecipe(new ItemStack(RidiculousBlocks.treePlanks, 4, x), new Object[] {new ItemStack(RidiculousBlocks.treeLogs,1,x)});
+            GameRegistry.addShapedRecipe(new ItemStack(RidiculousBlocks.craftBench, 1, x), new Object[] {"XX", "XX", 'X', new ItemStack(RidiculousBlocks.treePlanks, 1, x)});
         }
+        GameRegistry.addShapelessRecipe(new ItemStack(RidiculousBlocks.treePlanks, 4, 0), new Object[] {new ItemStack(RidiculousBlocks.spookyLog,1,0)});
         GameRegistry.addSmelting(new ItemStack(RidiculousBlocks.spookyLog, 1), new ItemStack(Items.coal, 1, 1), 0.15F);
-
     }
 
     public static void lexify(){
@@ -40,8 +37,8 @@ public class Crafter {
         OreDictionary.registerOre("treeLeaves", new ItemStack(RidiculousBlocks.treeLeaves, 1, OreDictionary.WILDCARD_VALUE));
         OreDictionary.registerOre("treeSapling", new ItemStack(RidiculousBlocks.treeSaplings, 1, OreDictionary.WILDCARD_VALUE));
         OreDictionary.registerOre("plankWood", new ItemStack(RidiculousBlocks.treePlanks, 1, OreDictionary.WILDCARD_VALUE));
-        OreDictionary.registerOre("crafterWood", new ItemStack(RidiculousBlocks.craftBench, 1, 0));
-        OreDictionary.registerOre("craftingTableWood", new ItemStack(RidiculousBlocks.craftBench, 1, 0));
+        OreDictionary.registerOre("crafterWood", new ItemStack(RidiculousBlocks.craftBench, 1, OreDictionary.WILDCARD_VALUE));
+        OreDictionary.registerOre("craftingTableWood", new ItemStack(RidiculousBlocks.craftBench, 1, OreDictionary.WILDCARD_VALUE));
 
     }
 
